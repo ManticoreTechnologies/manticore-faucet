1304f50466d0b86334807ad50e9b3bc720ca75bc
Manticore <development@manticore.technology>
2024-08-13 17:36:10 -0400

    Add ip settings option

--- manage_service.py
+++ manage_service.py
@@ -13,7 +13,7 @@ After=network.target
 User={config["Permission"]["user"]}
 Group={config["Permission"]["group"]}
 WorkingDirectory={os.getcwd()}
-ExecStart=gunicorn -w 1 -b 0.0.0.0:{config["General"]["port"]} startup:app
+ExecStart=gunicorn -w 1 -b {config["General"]["ip"]}:{config["General"]["port"]} startup:app
 Restart=always
 
 [Install]
